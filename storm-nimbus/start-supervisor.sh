ee725e297b0030c7f787510ed7ec730bd941de47
Cristian Barrientos Montoya <cs3kns@gmail.com>
2016-05-20 10:24:34 -0500

    added support for docker 1.11

--- storm-nimbus/start-supervisor.sh
+++ storm-nimbus/start-supervisor.sh
@@ -3,5 +3,7 @@ sed -i -e "s/%zookeeper%/zookeeper/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+echo nimbus
+cat $STORM_HOME/conf/storm.yaml
 
 /usr/sbin/sshd && supervisord
--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -3,4 +3,6 @@ sed -i -e "s/%nimbus%/nimbus/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
 
+cat $STORM_HOME/conf/storm.yaml
+
 /usr/sbin/sshd && supervisord
